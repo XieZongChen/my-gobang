0207865aae3c4dbb72d6b9b0068ce8f0bb6db75f
XieZongChen <952937570@qq.com>
2022-01-12 16:55:37 +0800

    feat: 部署

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import TDesign from 'tdesign-vue-next';
-import App from './App.vue'
-
-import 'tdesign-vue-next/es/style/index.css';
-
-const app = createApp(App);
-app.use(TDesign);
-app.mount('#app')
