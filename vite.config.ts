10f08cccb5b9892e2e339f7bf2db0214dd1e2bb6
XieZongChen <952937570@qq.com>
2022-01-12 17:02:31 +0800

    feat: 修复打包

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()]
+  plugins: [vue(), vueJsx()],
+  base: './',
 })
